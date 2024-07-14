4f2f1c9ea39c36fe52b71044a81a76bac0cfea60
Marchioro <olikujy03@gmail.com>
2024-07-14 17:37:26 -0300

    enio projeto html

--- main.js
+++ main.js
@@ -61,3 +61,89 @@ ScrollReveal().reveal('.qrcode', {
     duration: 1000,
     distance: '20%'
 });
+
+ScrollReveal().reveal('#video-source', {
+    origin: 'right',
+    duration: 1500,
+    distance: '20%'
+});
+ScrollReveal().reveal('#video-player', {
+    origin: 'right',
+    duration: 1500,
+    distance: '20%'
+});
+
+
+
+
+// Seleciona os elementos de menu
+// Captura os elementos de menu e vídeo
+let todos = document.getElementById('todos');
+let topLink = document.getElementById('top');
+let adc = document.getElementById('adc');
+let mid = document.getElementById('mid');
+let sup = document.getElementById('sup');
+let jun = document.getElementById('jun');
+let videoPlayer = document.getElementById('video-player');
+let videoSource = document.getElementById('video-source');
+
+// Adiciona ouvintes de evento para cada link de filtro
+todos.addEventListener('click', function(event) {
+    event.preventDefault(); // Evita o comportamento padrão de recarregar a página
+    trocarVideo('video/rota_do_meio.mp4');
+    videoPlayer.play();
+    
+});
+
+topLink.addEventListener('click', function(event) {
+    event.preventDefault(); // Evita o comportamento padrão de recarregar a página
+    trocarVideo('video/rota_do_meio.mp4');
+    videoPlayer.play();
+    ScrollReveal().reveal('#video-source', { delay: 500 });
+    ScrollReveal().reveal('#video-player', { delay: 500 });
+});
+
+adc.addEventListener('click', function(event) {
+    event.preventDefault(); // Evita o comportamento padrão de recarregar a página
+    trocarVideo('video/rota_do_jungle.mp4');
+    videoPlayer.play();
+});
+
+mid.addEventListener('click', function(event) {
+    event.preventDefault(); // Evita o comportamento padrão de recarregar a página
+    trocarVideo('video/rota_do_meio.mp4');
+    videoPlayer.play();
+});
+
+sup.addEventListener('click', function(event) {
+    event.preventDefault(); // Evita o comportamento padrão de recarregar a página
+    trocarVideo('video/rota_do_meio.mp4');
+    videoPlayer.play();
+});
+
+jun.addEventListener('click', function(event) {
+    event.preventDefault(); // Evita o comportamento padrão de recarregar a página
+    trocarVideo('video/rota_do_meio.mp4');
+    videoPlayer.play();
+});
+
+// Função para trocar o vídeo
+// Função para trocar o vídeo
+function trocarVideo(source) {
+    // Oculta o vídeo atual suavemente
+   
+    
+    // Aguarda um curto período para a transição ocorrer
+    setTimeout(() => {
+        videoSource.src = source; // Altera a origem do vídeo
+        videoPlayer.load(); // Recarrega o vídeo com a nova origem
+        videoPlayer.play(); // Inicia a reprodução do vídeo automaticamente
+        
+        
+        // Exibe o novo vídeo suavemente
+        
+    }, 500); // Tempo deve corresponder ao tempo de transição CSS
+   
+}
+
+
